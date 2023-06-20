2e6f69346c887a2e0d2a80bf26f22ef92523f291
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-06-20 21:11:57 +0900

    remove set_protocol from mount callback

--- keyboards/keyboard_quantizer/mini/matrix.c
+++ keyboards/keyboard_quantizer/mini/matrix.c
@@ -84,11 +84,6 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     }
 }
 
-static volatile bool set_protocol_complete = false;
-void                 tuh_hid_set_protocol_complete_cb(uint8_t dev_addr, uint8_t instance, uint8_t protocol) {
-    set_protocol_complete = true;
-}
-
 void tuh_mount_cb(uint8_t dev_addr) {
     if (led_count < 0) {
         led_count = timer_read();
@@ -96,18 +91,6 @@ void tuh_mount_cb(uint8_t dev_addr) {
 
     for (int instance = 0; instance < tuh_hid_instance_count(dev_addr);
          instance++) {
-        if (tuh_hid_get_protocol(dev_addr, instance) == HID_PROTOCOL_BOOT) {
-            set_protocol_complete = false;
-            __compiler_memory_barrier();
-            tuh_hid_set_protocol(dev_addr, instance, HID_PROTOCOL_REPORT);
-
-            uint16_t timeout_ms = 0;
-            while (!set_protocol_complete && ++timeout_ms < 100) {
-                wait_ms(1);
-                tuh_task();
-            }
-        }
-
         uint8_t const itf_protocol =
             tuh_hid_interface_protocol(dev_addr, instance);
         if (itf_protocol == HID_ITF_PROTOCOL_KEYBOARD) {
